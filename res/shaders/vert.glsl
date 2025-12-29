3090c0a8bf0d606ff89a79e99fcd1dc230a90e0e
Shegy <149938356+sShegy@users.noreply.github.com>
2025-12-29 16:11:01 +0100

    .

--- res/shaders/vert.glsl
+++ res/shaders/vert.glsl
@@ -3,23 +3,20 @@ layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aNormal;
 layout (location = 2) in vec2 aTexCoords;
 layout (location = 3) in vec3 aColor;
-
 out vec3 FragPos;
 out vec3 Normal;
 out vec2 TexCoords;
-out vec3 ObjectColor;
+out vec3 FaceColor;
 
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
-void main()
-{
+void main() {
     FragPos = vec3(model * vec4(aPos, 1.0));
-    // Ispravka normala ako se kocka skalira
     Normal = mat3(transpose(inverse(model))) * aNormal;
     TexCoords = aTexCoords;
-    ObjectColor = aColor;
+    FaceColor = aColor;
 
     gl_Position = projection * view * vec4(FragPos, 1.0);
 }
